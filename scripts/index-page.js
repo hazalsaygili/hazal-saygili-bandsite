923063f5b2730b1b86ac9157a8e8e95a0c486799
Hazal <hazalsaygili96@gmail.com>
2024-07-01 16:17:15 -0400

    update bio page adding comments

---
+++ scripts/index-page.js
@@ -0,0 +1,40 @@
+const form = document.querySelector(".commentFlow__addComment");
+console.log(form);
+
+// add an event listener ("callback")
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("form submitted");
+  console.log(event);
+
+  const name = event.target.commentName.value;
+  const content = event.target.commentContent.value;
+
+  const commentCardWrapper = document.createElement("div");
+  commentCardWrapper.classList.add("commentFlow__commentCardWrapper");
+
+  const commentImg = document.createElement("img");
+  commentCardWrapper.appendChild(commentImg);
+  commentImg.classList.add("commentFlow__Img");
+  commentImg.src = "./assets/images/Mohan-muruge.jpg";
+
+  const commentCard = document.createElement("li");
+  commentCard.classList.add("commentFlow__commentCard");
+  commentCardWrapper.appendChild(commentCard);
+
+  const commentName = document.createElement("p");
+  commentName.classList.add("commentFlow__commentedName");
+  commentName.innerHTML = name;
+  commentCard.appendChild(commentName);
+
+  const commentContent = document.createElement("p");
+  commentContent.classList.add("commentFlow__commentedContent");
+  commentContent.innerText = content;
+  commentCard.appendChild(commentContent);
+
+  const commentList = document.getElementById("comments")
+  commentList.insertBefore(commentCardWrapper,commentList.firstChild);
+
+});
+
+
